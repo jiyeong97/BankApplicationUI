2583c8fe819088e165832cede821dfd68804f09d
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-08-29 20:56:40 -0600

    OwnWideget

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widegets/button.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: Padding( 
-          padding: EdgeInsets.symmetric(horizontal: 40),
-          child:Column(
-            crossAxisAlignment:CrossAxisAlignment.start,
-          children: [
-           SizedBox(
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
                 height: 80,
               ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                Text('Hey, June',
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w800,
-                  ),
-                  ),
-                Text('Welcome back',
-                  style: TextStyle(
-                    color:Colors.white.withOpacity(0.8),
-                    fontSize: 18
-                  ),
-                  ),
-              ],)
-            ],),
-            SizedBox(height: 120,
-            ),
-            Text('Total Balance',
-              style:TextStyle(
-                fontSize: 22,
-                color:Colors.white.withOpacity(0.8),
-              ),
-              ),
-            SizedBox(height: 5,),
-            Text('\$5 194 482',
-              style:TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.w600,
-                color:Colors.white,
-              ),
-              ),
-              SizedBox(height: 20,),
               Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 18,
-                        horizontal: 50,
-                    ),
-                    child: Text('Trasnfer',
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                    ),
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Hey, June',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                      ),
+                    ],
                   )
                 ],
-                )
-          ],
-        ),
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button(
+                      text: 'Transfer',
+                      bgColor: Color(0xFFF1B33B),
+                      textColor: Colors.black,),
+                  Button(
+                      text: 'Request',
+                      bgColor: Color(0xFF1F2123),
+                      textColor: Colors.white,),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
