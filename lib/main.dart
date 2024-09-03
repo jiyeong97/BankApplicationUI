75e09213139e33dab8fcac90b18c001d83bd35e1
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-09-02 18:27:16 -0600

    card icon adding

--- lib/main.dart
+++ lib/main.dart
@@ -111,47 +111,65 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
-                  color:const Color(0xFF1F2123),
+                  color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(25),
                 ),
-                child: 
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 20, horizontal: 20
-                  ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                   child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text('Euro',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
-                          ),
-                        ),
-                        const SizedBox(height: 10,),
-                        Row(
-                          children: [
-                            const Text('6 428',
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Euro',
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 20,
+                              fontSize: 32,
                             ),
-                            ),
-                            const SizedBox(width: 10,),
-                            Text('EUR',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 20
-                            ),
-                            ),
-                        ],)
-                      ],
-                    )
-                  ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '6 428',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
                                 ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'EUR',
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: 20),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                     Transform.scale(
+                      scale: 2.2,
+                      child:  Transform.translate(
+                        offset: const Offset(-5, 12),
+                        child: const Icon(
+                          Icons.euro_rounded,
+                          color: Colors.white,
+                          size: 88,
+                        ),
+                      ),
+                     )
+                    ],
+                  ),
                 ),
               ),
             ],
